df07960cc3566acc2665c55d0c2247cbac55223b
MING YIN （殷明） <mingyin@trip.com>
2021-11-11 18:47:28 +0800

    webserver v3

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,10 @@ use std::thread;
 
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:7878").unwrap();
+    let pool = ThreadPool::new(10);
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        thread::spawn(|| {
+        pool.execute(|| {
             handle_connection(stream);
         });
     }
@@ -26,4 +27,85 @@ fn handle_connection(mut stream: TcpStream) {
     );
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
+}
+
+
+
+
+use std::{sync::{Arc, Mutex, mpsc}};
+
+enum Message {
+    NewJob(Job),
+    Terminate,
+}
+struct Worker {
+    id:usize,
+    thread:Option<thread::JoinHandle<()>>,
+}
+
+impl Worker {
+     fn new(id:usize,receiver:Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+           let message = receiver.lock().unwrap().recv().unwrap();
+           match message {
+               Message::NewJob(job) => {
+                 println!("Worker {} got a job; executing.",id);
+                 job();
+               }
+               Message::Terminate => {
+                   println!("Worker {} was told to terminate.",id);
+                   break;
+               }
+           }
+           
+        });
+        Worker {
+            id,
+            thread:Some(thread)
+        }
+    }
+}
+
+pub struct ThreadPool{
+    workers: Vec<Worker>,
+    sender:mpsc::Sender<Message>,
+}
+
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+impl ThreadPool {
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+        let mut workers = Vec::with_capacity(size);
+        let (sender,receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+        for n in 0..size {
+            workers.push(Worker::new(n,Arc::clone(&receiver)));
+        }
+        ThreadPool { workers,sender }
+    }
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+        self.sender.send(Message::NewJob(job)).unwrap();
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        println!("Sending terminate message to all workers.");
+        for _ in &self.workers {
+            self.sender.send(Message::Terminate).unwrap();
+        }
+        println!("Shutting down all workers.");
+        for worker in &mut self.workers {
+            println!("Shutting down worker {}",worker.id);
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
+    }
 }
